01d9baf2259016b540c5827242b343d1198d5ba6
user <user@users-MacBook-Pro.local>
2019-07-26 22:45:30 +0800

    删除链表中的节点

---
+++ src/删除链表中的节点[链表]/java/DeleteNodeFromLinkedList.java
@@ -0,0 +1,25 @@
+package com.leetcode;
+
+public class DeleteNodeFromLinkedList {
+
+    /**
+     *
+     * @param node
+     */
+    public void deleteNode(ListNode node) {
+            node.val = node.next.val;
+            node.next = node.next.next;
+    }
+
+}
+
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+
---
+++ src/删除链表中的节点[链表]/js/index.js
@@ -0,0 +1,13 @@
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function (node) {
+    node.val = node.next.val;
+    node.next = node.next.next;
+};
